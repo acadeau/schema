7f4ab2d82498cdf01c7956fdf37b0650854f6c17
Alexandre Cadeau <alexandre@obole.eu>
2021-10-26 21:25:27 +0200

    deps: update to beta

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("schema")
+        .subcommand(
+            App::new("add") //
+                .about("Adds files to myapp")
+                .version("0.1")
+                .author("Kevin K.")
+                .arg(
+                    Arg::new("input")
+                        .about("the file to add")
+                        .index(1)
+                        .required(true),
+                ),
+        )
+        .get_matches();
 }

9d5106fc704f81c222b285ed37bda555a4e3297c
Alexandre Cadeau <alexandre@obole.eu>
2021-10-26 21:26:51 +0200

    feat: initial command

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,10 @@
-use clap::{App, Arg};
+use clap::App;
 
 fn main() {
     let matches = App::new("schema")
         .subcommand(
-            App::new("add") //
-                .about("Adds files to myapp")
-                .version("0.1")
-                .author("Kevin K.")
-                .arg(
-                    Arg::new("input")
-                        .about("the file to add")
-                        .index(1)
-                        .required(true),
-                ),
+            App::new("setup") //
+                .about("Setup database to receive schema change"),
         )
         .get_matches();
 }

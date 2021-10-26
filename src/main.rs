7cca5230027e0b85164a01358a104e02c53a98d1
Alexandre Cadeau <alexandre@obole.eu>
2021-10-26 22:05:08 +0200

    feat: add integration test to setup schema

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,64 @@
 use clap::App;
 
+fn setup_schema(database_uri: &str) -> bool {
+    true
+}
+
 fn main() {
-    let matches = App::new("schema")
+    let _matches = App::new("schema")
         .subcommand(
             App::new("setup") //
                 .about("Setup database to receive schema change"),
         )
         .get_matches();
+}
+
+#[cfg(test)]
+mod tests {
+    use testcontainers::{
+        clients,
+        images::{self, generic::WaitFor},
+        Docker,
+    };
+
+    #[test]
+    fn setup_tool() {
+        let docker = clients::Cli::default();
+
+        let db = "test";
+        let user = "user-test";
+        let password = "user-password";
+
+        let generic_postgres = images::generic::GenericImage::new("postgres:14-alpine")
+            .with_wait_for(WaitFor::message_on_stderr(
+                "database system is ready to accept connections",
+            ))
+            .with_env_var("POSTGRES_DB", db)
+            .with_env_var("POSTGRES_USER", user)
+            .with_env_var("POSTGRES_PASSWORD", password);
+
+        let node = docker.run(generic_postgres);
+
+        let connection_string = &format!(
+            "postgres://{}:{}@localhost:{}/{}",
+            user,
+            password,
+            node.get_host_port(5432)
+                .expect("container can't retrieve port"),
+            db
+        );
+
+        super::setup_schema(connection_string);
+
+        let mut conn = postgres::Client::connect(connection_string, postgres::NoTls).unwrap();
+
+        let check_schema_created = conn
+            .query(
+                "SELECT EXISTS(SELECT 1 FROM pg_namespace WHERE nspname = 'db_state');",
+                &[],
+            )
+            .unwrap();
+        assert_eq!(check_schema_created.len(), 1);
+        assert!(check_schema_created[0].get::<usize, bool>(0));
+    }
 }
